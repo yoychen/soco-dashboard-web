b63d95c3eaf675548000b7697f5dcddb5a27ea73
sakuxz <yui12327@gmail.com>
2016-08-07 18:07:27 +0800

    modify manufacture table

--- src/Pages/ManufactureTr.jsx
+++ src/Pages/ManufactureTr.jsx
@@ -40,12 +40,19 @@ var ManufactureTr = React.createClass({
     return (
       <tbody className={this.props.done?'done':''}>
         <tr>
-          <td rowSpan="2">{d.manu_id}</td>
-          <td rowSpan="2">{d.prod_no}</td>
-          <td rowSpan="2">{d.predict_date}</td>
-          <td rowSpan="2">{d.source_date}</td>
-          <td>H1</td>
-          <td>
+          <td rowSpan="3">{d.manu_id}</td>
+          <td rowSpan="3">{d.prod_no}</td>
+          <td rowSpan="3">{d.predict_date}</td>
+          <td rowSpan="3">{d.source_date}</td>
+          <td colSpan="2" >H1</td>
+          <td colSpan="2" >H2</td>
+          <td colSpan="2" >H3</td>
+          <td colSpan="2" >H4</td>
+          <td rowSpan="3">首件</td>
+          <td rowSpan="3">完成日</td>
+        </tr>
+        <tr>
+          <td colSpan="2">
             <select name="machine" className="ui " id="select">
               <option value="" defaultChecked={true} >{d.step[0].machine}</option>
               <option value="">H03</option>
@@ -54,14 +61,33 @@ var ManufactureTr = React.createClass({
               <option value="">H06</option>
             </select>
           </td>
-          <td>H2</td>
-          <td>{d.step[1].machine}</td>
-          <td>H3</td>
-          <td>{d.step[2].machine}</td>
-          <td>H4</td>
-          <td>{d.step[3].machine}</td>
-          <td rowSpan="2">首件</td>
-          <td rowSpan="2">完成日</td>
+          <td colSpan="2">
+            <select name="machine" className="ui " id="select">
+              <option value="" defaultChecked={true} >{d.step[1].machine}</option>
+              <option value="">H03</option>
+              <option value="">H04</option>
+              <option value="">H05</option>
+              <option value="">H06</option>
+            </select>
+          </td>
+          <td colSpan="2">
+            <select name="machine" className="ui " id="select">
+              <option value="" defaultChecked={true} >{d.step[2].machine}</option>
+              <option value="">H03</option>
+              <option value="">H04</option>
+              <option value="">H05</option>
+              <option value="">H06</option>
+            </select>
+          </td>
+          <td colSpan="2">
+            <select name="machine" className="ui " id="select">
+              <option value="" defaultChecked={true} >{d.step[3].machine}</option>
+              <option value="">H03</option>
+              <option value="">H04</option>
+              <option value="">H05</option>
+              <option value="">H06</option>
+            </select>
+          </td>
         </tr>
         <tr>
           <td>{d.step[0].predict_num}</td>
